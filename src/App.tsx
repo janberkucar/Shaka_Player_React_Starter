426cd19460f7f1eda7c6f55569531e444be4a307
Janberk Uçar <canberk.ucar.mail@gmail.com>
2022-03-25 17:04:21 +0300

    Initial shaka_react_starter commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ShakaPlayerWrapper from "./components/ShakaPlayerWrapper";
 
 function App() {
+  // const license = config.video.license;
+  // const manifest = config.video.manifest;
+  // const subtitle = config.video.subtitle;
+
+  const license = "License from env";
+  const manifest = "Manifest from env";
+  const subtitle = "Subtitle from env";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-100">
+      <div className="d-flex flex-column align-items-center h-100 my-auto">
+        <div className="d-flex">
+          <h1>Shaka player starter kit demo</h1>
+        </div>
+        <div className="d-flex h-100">
+          <ShakaPlayerWrapper
+            license={license}
+            manifest={manifest}
+            subtitle={subtitle}
+          ></ShakaPlayerWrapper>
+        </div>
+      </div>
     </div>
   );
 }
